ad10266c524137882f6735b00fc4c22b2e6e1073
Joe Hoyle <joehoyle@gmail.com>
2019-04-15 10:10:40 +0700

    Add check for add_action
    Per https://github.com/humanmade/platform-dev/issues/323, we are screwing up any inclusion of autoload.php outside of the wp-config.php. We are discussing a better long term solution in https://github.com/humanmade/platform-dev/pull/325, but this is intended to get things functioning.

--- load.php
+++ load.php
@@ -4,6 +4,11 @@ namespace HM\Platform\Multilingual;
 
 use HM\Platform;
 
+// Don't self-initialize if this is not a Platform execution.
+if ( ! function_exists( 'add_action' ) ) {
+	return;
+}
+
 add_action( 'hm-platform.modules.init', function () {
 	Platform\register_module( 'multilingual', __DIR__, 'Multilingual', [] );
 } );
