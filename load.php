fce1854fa259dd0978bac5f9859f5aa41f4c90e0
Robert O'Rourke <robert@o-rourke.org>
2019-10-01 14:55:18 +0100

    Support new autoload method

--- load.php
+++ load.php
@@ -4,11 +4,6 @@ namespace Altis\Multilingual; // @codingStandardsIgnoreLine
 
 use function Altis\register_module;
 
-// Don't self-initialize if this is not an Altis execution.
-if ( ! function_exists( 'add_action' ) ) {
-	return;
-}
-
 add_action( 'altis.modules.init', function () {
 	register_module( 'multilingual', __DIR__, 'Multilingual', [] );
 } );
