d692b67f0ee2bb6bafc3ab8539bf0d5b6f16f825
Robert O'Rourke <robert@o-rourke.org>
2020-02-17 12:34:16 +0000

    split out register callback before function guard

--- load.php
+++ load.php
@@ -4,11 +4,13 @@ namespace Altis\Multilingual; // @codingStandardsIgnoreLine
 
 use function Altis\register_module;
 
+function register() {
+	register_module( 'multilingual', __DIR__, 'Multilingual', [] );
+}
+
 // Don't self-initialize if this is not an Altis execution.
 if ( ! function_exists( 'add_action' ) ) {
 	return;
 }
 
-add_action( 'altis.modules.init', function () {
-	register_module( 'multilingual', __DIR__, 'Multilingual', [] );
-} );
+add_action( 'altis.modules.init', __NAMESPACE__ . '\\register' );
